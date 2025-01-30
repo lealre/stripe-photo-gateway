3e7be4ab16b9f94e0930530d73a00205ab8ef354
lealre <renaanvp@gmail.com>
2025-01-30 19:22:11 +0000

    tests: refactor `conftest.py` into a single file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import base64
+import os
 from collections.abc import AsyncGenerator, Generator
+from dataclasses import dataclass
 from typing import Literal
 
 import boto3
@@ -14,6 +17,12 @@ from testcontainers.redis import AsyncRedisContainer
 from src.api.dependencies import get_redis, get_session
 from src.app import app
 from src.models import Base
+from src.schemas.orders import (
+    CustomerInfo,
+    OrderRequestPayload,
+    PhotoDetails,
+    PhotosUploadPayload,
+)
 
 BASE_URL = 'http://test'
 
@@ -88,3 +97,85 @@ def s3_client() -> Generator[client.BaseClient, None, None]:
     with mock_aws():
         s3_client = boto3.client('s3')
         yield s3_client
+
+
+@dataclass
+class EncodedPhotoDataClass:
+    fileName: str
+    fileType: str
+    encodedData: str
+
+
+@pytest.fixture(scope='session')
+def encoded_photos() -> list[EncodedPhotoDataClass]:
+    photos_test_directory = 'tests/utils/photos'
+
+    photos_path = [
+        os.path.join(photos_test_directory, file)
+        for file in os.listdir(photos_test_directory)
+        if os.path.isfile(os.path.join(photos_test_directory, file))
+    ]
+
+    encoded_files = []
+
+    for file_path in photos_path:
+        with open(file_path, 'rb') as file:
+            file_content = file.read()
+
+            file_extension = os.path.splitext(file_path)[1].lower()
+            if file_extension == '.png':
+                mime_type = 'image/png'
+            elif file_extension in {'.jpg', '.jpeg'}:
+                mime_type = 'image/jpeg'
+            else:
+                mime_type = 'application/octet-stream'
+
+            encoded_content = base64.b64encode(file_content).decode('utf-8')
+
+            encoded_photo = EncodedPhotoDataClass(
+                fileName=os.path.basename(file_path),
+                fileType=mime_type,
+                encodedData=f'data:{mime_type};base64,{encoded_content}',
+            )
+
+            encoded_files.append(encoded_photo)
+
+    return encoded_files
+
+
+@pytest.fixture
+def photos_upload_order(
+    encoded_photos: list[EncodedPhotoDataClass],
+) -> PhotosUploadPayload:
+    photos_details_schema: list[PhotoDetails] = []
+
+    for photo in encoded_photos:
+        photos_details_schema.append(
+            PhotoDetails(
+                fileName=photo.fileName,
+                base64=photo.encodedData,
+                quantity=1,
+                fileType=photo.fileType,
+            )
+        )
+
+    order_info_schema = PhotosUploadPayload(photos=photos_details_schema)
+
+    return order_info_schema
+
+
+@pytest.fixture
+def payload_order(photos_upload_order: PhotosUploadPayload) -> OrderRequestPayload:
+    customer_info = CustomerInfo(
+        customerEmail='test@email.com',
+        phoneNumber='1234567890',
+        formattedAddress='123 Main St, Example City, 98765',
+    )
+
+    payload = OrderRequestPayload(
+        orderId=1,
+        customerInfo=customer_info,
+        orderInfo=photos_upload_order,
+    )
+
+    return payload
